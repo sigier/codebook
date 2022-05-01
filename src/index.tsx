60e2cf91e6a152503d3e3388a513906d188604ef
sigier <sergiigolinka@gmail.com>
2022-05-01 13:25:26 +0300

    transpiler added tp the code

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,34 @@
 import  ReactDOM  from 'react-dom';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import * as esbuild from 'esbuild-wasm';
 
 const App = () => {
+    const ref = useRef<any>();
     const [input, setInput] = useState('');
     const [code, setCode] = useState('');
+
+    const startService = async() => {
+        ref.current = await esbuild.startService({
+            worker: true,
+            wasmURL: '/esbuild.wasm'
+        });
+
+        
+    }
+
+    useEffect(() => { startService()},[]);
     
-    const onClick = () => {
+    const onClick = async () => {
+        if (!ref.current) {
+            return;
+        }
+
+        const result = await ref.current.transform(input, {
+            loader: 'jsx',
+            target: 'es2015'
+        });
+
+        setCode(result.code);
         
     };
     
