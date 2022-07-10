de0591ca89e6135a628f1f7b5d7104f1eda1feee
sigier <sergiigolinka@gmail.com>
2022-07-10 14:50:13 +0300

    Resizable-element style improved

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,33 +1,37 @@
-import React, { useState } from 'react';
-
+import { useState, useEffect } from 'react';
 import CodeEditor from './code-editor';
 import Preview from './preview';
-import bundler from '../bundler';
-import Resizable  from './resizable';
-
+import bundle from '../bundler';
+import Resizable from './resizable';
 
 const CodeCell = () => {
-    const [input, setInput] = useState('');
-    const [code, setCode] = useState('');  
-    
-    const onClick = async () => {
-        const output = await bundler(input);
-        setCode(output);
-    };
+  const [code, setCode] = useState('');
+  const [input, setInput] = useState('');
 
-    
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      const output = await bundle(input);
+      setCode(output);
+    }, 750);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [input]);
+
   return (
-    <Resizable direction={'vertical'}>
-      <div style={{height: '100%', display: 'flex', flexDirection: 'row'}}>
-      <CodeEditor
-        initialValue="const a = 1;"
-        onChange={(value) => setInput(value)}
-      />
-      <Preview code={code} />
-    </div>
-  </Resizable>
+    <Resizable direction="vertical">
+      <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
+        <Resizable direction="horizontal">
+          <CodeEditor
+            initialValue="const a = 1;"
+            onChange={(value) => setInput(value)}
+          />
+        </Resizable>
+        <Preview code={code} />
+      </div>
+    </Resizable>
   );
 };
-
 
 export default CodeCell;
