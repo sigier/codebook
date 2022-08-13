713b0fa99963491f1a2e1e7eb72bc4ece7d7f8af
sigier <sergiigolinka@gmail.com>
2022-08-13 22:40:52 +0300

    Initial Store Action setup added

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,20 @@
 import  ReactDOM  from 'react-dom';
 import  'bulmaswatch/superhero/bulmaswatch.min.css';
 import TextEditor from './components/text-editor';
+import { Provider } from 'react-redux';
+import { store } from './state';
+
 
 
 const App = () => { 
     
   return (
-    <div>
-      <TextEditor/>
-    </div>
+    <Provider store={store}>
+      <div>
+        <TextEditor/>
+      </div>
+    </Provider>
+
   );
 };
 
