d67d8f323ce223334455ba6f59c14a678130b3d6
sigier <sergiigolinka@gmail.com>
2022-08-17 23:36:49 +0300

    Immer added to reducer

--- src/state/reducers/cellReducer.ts
+++ src/state/reducers/cellReducer.ts
@@ -1,7 +1,9 @@
-import { act } from 'react-dom/test-utils';
+import { Immer } from 'immer';
 import { ActionType } from '../action-types';
 import { Action } from '../actions';
 import { Cell } from '../cell';
+import produce from 'immer';
+import { getBreaksNeededForEmptyLineAfter } from '@uiw/react-md-editor/lib/cjs/utils/markdownUtils';
 
 interface CellsState{
     loading: boolean;
@@ -20,25 +22,51 @@ const initialState : CellsState = {
      
 }
 
-const reducer = (state: CellsState = initialState, action: Action): CellsState => {
+const reducer = produce( (state: CellsState = initialState, action: Action) => {
     switch (action.type){
         case ActionType.UPDATE_CELL:
 
             const { id, content } = action.payload;
-            return {...state, data: {
-                ...state.data, [id]: {
-                    ...state.data[id], content
-                }
-            }};
+            state.data[id].content = content;
+            return state;
         case ActionType.DELETE_CELL:
+            delete state.data[action.payload];
             return state;
         case ActionType.INSERT_CELL_BEFORE:
+            const cell : Cell = {
+                content: '',
+                type: action.payload.type,
+                id:  randomId()
+            };
+
+            state.data[cell.id] = cell;
+            const foundIndex = state.order.findIndex(id => id === action.payload.id);
+
+            if (foundIndex < 0){
+                state.order.push(cell.id);
+            }else{
+                state.order.splice(foundIndex, 0, cell.id);
+            }
             return state;
+
         case ActionType.MOVE_CELL:
+            const { direction } = action.payload;
+            const index = state.order.findIndex( id => id === action.payload.id);
+            const targetIndex = direction === 'up' ? index - 1 : index + 1;
+
+            if (targetIndex < 0 || targetIndex > state.order.length-1){
+                return;
+            }
+            state.order[index] = state.order[targetIndex];
+            state.order[targetIndex] = action.payload.id;
             return state;
         default:
             return state;
     }
+} );
+
+const randomId = () => {
+    return Math.random().toString(36).substr(2,5);
 };
 
 export default reducer;
